2539643bcfaa185c60a6b4c0e59d335ecdfb0fda
mohamedrishal <rishalmundekkat@gmail.com>
2023-11-18 10:42:20 +0530

    project add with img

--- Routes/router.js
+++ Routes/router.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const userController = require('../Controllers/userController')
 const jwtMiddleware = require('../Middlewares/jwtMiddleware')
 const projectController = require('../Controllers/projectController')
+const multerConfig = require('../Middlewares/multerMiddleware')
 
 
 
@@ -15,7 +16,10 @@ router.post('/user/login',userController.login)
 
 
 // add-project
-router.post('/projects/add',jwtMiddleware,projectController.addProjects)
+router.post('/projects/add',jwtMiddleware,multerConfig.single('projectImage'),
+projectController.addProjects
+)
+
 
 
 // export server
